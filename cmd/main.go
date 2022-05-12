1f0d5004f7e987d2010de7ccdbc9dd9a8b1d9981
gorobot-nz <gorobot.nz@gmail.com>
2022-05-13 00:09:47 +0300

    Pull docker images

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	dockerOperator "github.com/gorobot-nz/docker-operator/pkg/operator"
-	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	operator := dockerOperator.NewOperator()
 
-	containers := operator.Ps()
-
-	for _, container := range containers {
+	operator.Ps()
+	/*for _, container := range containers {
 		log.Println(container)
-	}
+	}*/
 
 	operator.ObserveContainers()
 }
--- pkg/operator/operator.go
+++ pkg/operator/operator.go
@@ -8,6 +8,9 @@ import (
 	dockerFilters "github.com/docker/docker/api/types/filters"
 	dockerClient "github.com/docker/docker/client"
 	log "github.com/sirupsen/logrus"
+	"io"
+	"os"
+	"strings"
 )
 
 const (
@@ -40,6 +43,22 @@ func (o *Operator) Ps() map[string]ContainerInfo {
 		o.containers[container.ID] = *NewContainerInfo(&container)
 	}
 	return o.containers
+}
+
+func (o *Operator) Images() {
+	ctx := context.Background()
+	filters := dockerFilters.NewArgs()
+	filters.Add("reference", "ghcr.io/slntopp/nocloud/apiserver-web")
+	images, err := o.client.ImageList(ctx, types.ImageListOptions{Filters: filters})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, image := range images {
+		fmt.Println(image.ID)
+		fmt.Println(image.RepoTags)
+		fmt.Println("/-------------------------------------------/")
+	}
 }
 
 func (o *Operator) ObserveContainers() {
@@ -76,10 +95,42 @@ func (o *Operator) processEvent(ctx context.Context, event events.Message) {
 	if err != nil {
 		log.Fatal("Error")
 	}
-	var container *ContainerInfo
-	container = NewContainerInfo(&containers[0])
+	container := NewContainerInfo(&containers[0])
 	o.containers[container.Id] = *container
 	log.Println("Container started")
 	log.Println(container)
+	o.CheckHash(ctx, container.Id)
 	return
+}
+
+func (o *Operator) CheckHash(ctx context.Context, containerId string) {
+	container, _, err := o.client.ContainerInspectWithRaw(ctx, containerId, false)
+	if err != nil {
+		return
+	}
+
+	image, _, err := o.client.ImageInspectWithRaw(ctx, container.Image)
+	if err != nil {
+		return
+	}
+
+	imageFullName := image.RepoTags[0]
+	imageName := getImageName(imageFullName)
+	o.pullImage(ctx, imageName)
+}
+
+func (o *Operator) pullImage(ctx context.Context, imageName string) {
+	out, err := o.client.ImagePull(ctx, imageName, types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	defer out.Close()
+
+	io.Copy(os.Stdout, out)
+}
+
+func getImageName(imageFullName string) string {
+	splitedName := strings.Split(imageFullName, ":")
+	return splitedName[0]
 }
